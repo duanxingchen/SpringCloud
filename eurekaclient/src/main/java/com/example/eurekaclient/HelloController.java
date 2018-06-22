e69d3b00d033984defcfe6e8db0e21cb31284ddb
duanxingchen <1041992747@qq.com>
2018-06-22 22:26:12 +0800

    整体提交

---
+++ eurekaclient/src/main/java/com/example/eurekaclient/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.eurekaclient;
+
+import com.netflix.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String index(){
+        return  "hello world";
+    }
+}
