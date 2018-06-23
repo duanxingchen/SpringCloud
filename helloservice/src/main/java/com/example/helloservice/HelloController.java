6011bb9e810b362abe26b37d86849122a80d6581
duanxingchen <1041992747@qq.com>
2018-06-23 21:12:58 +0800

    hystrix

--- helloservice/src/main/java/com/example/helloservice/HelloController.java
+++ helloservice/src/main/java/com/example/helloservice/HelloController.java
@@ -4,13 +4,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class HelloController {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String index(){
+    public String index() throws InterruptedException {
         System.out.print("3333333333");
         System.out.print("44444444");
+        int sleepTime = new Random().nextInt(3000);
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return  "hello world";
     }
 }
---
+++ ribbonconsumer/src/main/java/com/example/ribbonconsumer/HelloService.java
@@ -0,0 +1,26 @@
+package com.example.ribbonconsumer;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "helloFallback")
+    public String helloService(){
+        return  restTemplate.getForEntity("http://HELLO-SERVICE/hello",String.class).getBody();
+    }
+
+    public String helloFallback(){
+        return "error";
+    }
+}
